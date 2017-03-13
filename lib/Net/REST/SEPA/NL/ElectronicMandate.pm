b762aa7cb694b1ad36b80f9384089c913ee919f0
sebastiaan <sebastiaan@dev-1.nederhost.net>
2017-03-13 12:53:05 +0100

    Okay, we're doing this in a much smarter way now, or so I hope. This is what I get for fixing this on a production system.

--- lib/Net/REST/SEPA/NL/ElectronicMandate.pm
+++ lib/Net/REST/SEPA/NL/ElectronicMandate.pm
@@ -66,32 +66,37 @@ sub parse {
   # Sometimes the eMandate platform uses a namespace prefix even for it's
   # primary namespace. No big problem, but we do need to do some work.
 
-  my ( $root_elem ) = ( grep /^-/, ( keys %{$response} ))[0];
-  if ( $root_elem =~ /^([^:]+):(.+)$/ ) {
+  foreach my $r ( 'DirectoryRes', 'AcquirerTrxRes', 'AcquirerStatusRes' ) {
 
-    my $default_ns = $1;
-    sub rename_elements {
-      my $ns = shift;
-      my $in = shift;
-      if ( ref $in eq 'HASH' ) {
-        while ( my ( $k, $v ) = each %{$in} ) {
-          if ( $k =~ /^$ns:(.+)/ ) {
-            $in->{$1} = $v;
-            delete $in->{$k};
-            if ( ref $v ) {
-              rename_elements ( $ns, $v );
+    my ( $root_elem ) = ( grep /^(.+:)$r/, ( keys %{$response} ));
+    if ( $root_elem && ( $root_elem =~ /^([^:]+):(.+)$/ )) {
+
+      my $default_ns = $1;
+      sub rename_elements {
+        my $ns = shift;
+        my $in = shift;
+        if ( ref $in eq 'HASH' ) {
+          while ( my ( $k, $v ) = each %{$in} ) {
+            if ( $k =~ /^$ns:(.+)/ ) {
+              $in->{$1} = $v;
+              delete $in->{$k};
+              if ( ref $v ) {
+                rename_elements ( $ns, $v );
+              }
             }
           }
-        }
-      } elsif ( ref $in eq 'ARRAY' ) {
-        foreach my $e ( @{$in} ) {
-          rename_elements ( $ns, $e );
+        } elsif ( ref $in eq 'ARRAY' ) {
+          foreach my $e ( @{$in} ) {
+            rename_elements ( $ns, $e );
+          }
         }
       }
+
+      rename_elements ( $default_ns, $response );
+      
+      last;
+
     }
-
-    rename_elements ( $default_ns, $response );
-
   }
   
   # The default XML::Fast parser is pretty nice, but we do some additional
